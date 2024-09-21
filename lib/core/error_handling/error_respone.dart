acd80b0c32eac97e5ac371ebd36d8d52052dd768
nate <thcdzch@gmail.com>
2024-09-21 13:58:27 +0700

    change prombles...

--- lib/core/error_handling/error_respone.dart
+++ lib/core/error_handling/error_respone.dart
@@ -1,27 +1,27 @@
-import 'package:golobe/core/consttants/status_code.dart';
-import 'package:golobe/core/error_handling/exception_error.dart';
+// import 'package:golobe/core/consttants/status_code.dart';
+// import 'package:golobe/core/error_handling/exception_error.dart';
 
-class ErrorResponse extends ExceptionError {
-  ErrorResponse({
-    int? code,
-    String? message,
-  })  : this.code = code ?? StatusCode.errorUnknownCode,
-        this.messages = message ?? 'Đã có lỗi xảy ra, vui lòng thử lại sau.',
-        super(message ?? 'Đã có lỗi xảy ra, vui lòng thử lại sau.', code: code);
+// class ErrorResponse extends ExceptionError {
+//   ErrorResponse({
+//     int? code,
+//     String? message,
+//   })  : this.code = code ?? StatusCode.errorUnknownCode,
+//         this.messages = message ?? 'Đã có lỗi xảy ra, vui lòng thử lại sau.',
+//         super(message ?? 'Đã có lỗi xảy ra, vui lòng thử lại sau.', code: code);
 
-  final int? code;
-  final String? messages;
+//   final int? code;
+//   final String? messages;
 
-  factory ErrorResponse.fromJson(Map<String, dynamic> json) => ErrorResponse(
-        code: int.tryParse(json["code"]?.toString() ?? ''),
-        message: json["message"] ?? json['error'],
-      );
+//   factory ErrorResponse.fromJson(Map<String, dynamic> json) => ErrorResponse(
+//         code: int.tryParse(json["code"]?.toString() ?? ''),
+//         message: json["message"] ?? json['error'],
+//       );
 
-  Map<String, dynamic> toJson() => {
-        "code": code,
-        "message": messages,
-      };
+//   Map<String, dynamic> toJson() => {
+//         "code": code,
+//         "message": messages,
+//       };
 
-  @override
-  String toString() => '$messages\n[$code]';
-}
+//   @override
+//   String toString() => '$messages\n[$code]';
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:golobe/router/router.dart';
 import 'package:golobe/utils/colorsController/colors_controller.dart';
-import 'package:golobe/utils/fire-base/StoreDataFb.dart';
+import 'package:golobe/utils/fire-base/storeDataFb.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
